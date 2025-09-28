1485e7426b451f1134e35154ec264e82648aab10
unknown <Ali-Hakeem@github.com>
2025-09-28 09:21:53 +0700

    update main menu

--- src/app/page.js
+++ src/app/page.js
@@ -5,52 +5,40 @@ import { useRouter } from "next/navigation";
 export default function MenuPage() {
   const router = useRouter();
 
+  const menus = [
+    { name: "Update Skor", path: "/score" },
+    { name: "Quotes Editor", path: "/quotes" },
+    { name: "Info Editor", path: "/info" },
+    { name: "Statistik Editor", path: "/statistik" },
+    { name: "Klasmen", path: "/klasmen" },
+    { name: "Individual Stats", path: "/individual" },
+  ];
+
   return (
-    <main className="flex flex-col items-center justify-center min-h-screen gap-8 text-white bg-[#ff6508]">
-      <h1 className="text-4xl font-bold mb-6">Menu Editor</h1>
+    <main className="flex flex-col items-center justify-center min-h-screen px-6 bg-[#ff6508] text-white">
+      {/* Judul */}
+      <h1 className="text-5xl font-extrabold mb-10 drop-shadow-lg">
+        Menu Editor
+      </h1>
+      <div className="absolute flex justify-center items-center top-100">
+          <img
+            src="/assets/cf.png"
+            className="w-[120px] h-[120px] object-contain"
+            alt="CF Logo"
+          />
+        </div>
 
-      <div className="flex flex-col gap-6">
-        <button
-          onClick={() => router.push("/score")}
-          className=" font-bold px-8 py-4 rounded-lg hover:bg-gray-200"
-        >
-          Update Skor
-        </button>
-
-        <button
-          onClick={() => router.push("/quotes")}
-          className=" font-bold px-8 py-4 rounded-lg hover:bg-gray-200"
-        >
-          Quotes Editor
-        </button>
-
-        <button
-          onClick={() => router.push("/info")}
-          className=" font-bold px-8 py-4 rounded-lg hover:bg-gray-200"
-        >
-          Info Editor
-        </button>
-
-        <button
-          onClick={() => router.push("/statistik")}
-          className=" font-bold px-8 py-4 rounded-lg "
-        >
-          Statistik Editor
-        </button>
-
-        <button
-          onClick={() => router.push("/klasmen")}
-          className=" font-bold px-8 py-4 rounded-lg "
-        >
-          Klasmen
-        </button>
-
-        <button
-          onClick={() => router.push("/individual")}
-          className=" font-bold px-8 py-4 rounded-lg bg-cyan-950"
-        >
-          Individual Stats
-        </button>
+      {/* Container Buttons */}
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-6 w-full max-w-2xl">
+        {menus.map((menu, i) => (
+          <button
+            key={i}
+            onClick={() => router.push(menu.path)}
+            className="bg-white text-[#ff6508] font-bold text-xl py-6 px-6 rounded-2xl shadow-lg hover:scale-105 hover:shadow-2xl transition-all duration-200"
+          >
+            {menu.name}
+          </button>
+        ))}
       </div>
     </main>
   );
