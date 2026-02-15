eef8d151b1f9b9ae4e11e720302a6c4605204969
unknown <Ali-Hakeem@github.com>
2026-02-15 09:18:19 +0700

    update informasi & transfer post

--- src/data/overlayKompetisi.js
+++ src/data/overlayKompetisi.js
@@ -2,7 +2,7 @@ const overlayKompetisi = {
     inggris: ["Premier League.png", "FA-cup.png", "Carabao.png" ],
     eropa: ["UEFL.png", "UECL.png", "UCL.png", "WorldCup.png"],
     france: ["League1.png"],
-    spanyol: ["Laliga.png", "cdr.jpeg"],
+    spanyol: ["Laliga.png", "cdr.png"],
     italia: ["Seria A.png", "coppa.png"],
     jerman: ["Bundesliga.png"],
     belanda: ["Liga belanda.png"],
